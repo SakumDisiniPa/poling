696bee0f265f16269decba8c8298c98299861394
SakumDisiniPa <sakumtala8931@gmail.com>
2025-08-02 18:49:08 +0700

    terbaru;coy

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -35,14 +35,6 @@ class LoginPage extends StatelessWidget {
                           color: Color(0xFF673AB7),
                         ),
                       ),
-                      const SizedBox(height: 8),
-                      Text(
-                        'Voting made simple',
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.grey[600],
-                        ),
-                      ),
                     ],
                   ),
                   const SizedBox(height: 40),
@@ -102,22 +94,7 @@ class LoginPage extends StatelessWidget {
                     ),
                     style: const TextStyle(color: Colors.black87),
                   ),
-                  const SizedBox(height: 10),
-                  
-                  // Forgot Password
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        'Lupa kata sandi?',
-                        style: TextStyle(
-                          color: Colors.grey[600],
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 20),
+                  const SizedBox(height: 30),
                   
                   // Login Button
                   ElevatedButton(
@@ -140,76 +117,6 @@ class LoginPage extends StatelessWidget {
                         fontWeight: FontWeight.bold,
                         color: Colors.white),
                     ),
-                  ),
-                  const SizedBox(height: 30),
-                  
-                  // Divider with "or"
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Divider(
-                          color: Colors.grey[400],
-                          thickness: 1,
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 16),
-                        child: Text(
-                          'ATAU',
-                          style: TextStyle(
-                            color: Colors.grey[600],
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                      Expanded(
-                        child: Divider(
-                          color: Colors.grey[400],
-                          thickness: 1,
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 30),
-                  
-                  // Social Login Options
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      IconButton(
-                        icon: Image.asset('assets/google_icon.png'),
-                        iconSize: 50,
-                        onPressed: () {},
-                      ),
-                      const SizedBox(width: 20),
-                      IconButton(
-                        icon: Image.asset('assets/facebook_icon.png'),
-                        iconSize: 50,
-                        onPressed: () {},
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 20),
-                  
-                  // Sign Up Prompt
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Belum punya akun? ',
-                        style: TextStyle(color: Colors.grey[600]),
-                      ),
-                      TextButton(
-                        onPressed: () {},
-                        child: const Text(
-                          'Daftar sekarang',
-                          style: TextStyle(
-                            color: Color(0xFF673AB7),
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    ],
                   ),
                 ],
               ),
